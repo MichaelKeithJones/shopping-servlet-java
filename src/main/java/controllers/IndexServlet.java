0e2ebda41e807e48a7bcfa805f01a3991584dc7d
Michael Jones <michael.k.jones.20@gmail.com>
2022-04-27 11:34:24 -0500

    Index page controller and view working.

--- src/main/java/controllers/IndexServlet.java
+++ src/main/java/controllers/IndexServlet.java
@@ -1,16 +1,16 @@
+package controllers;
+
 import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
-@WebServlet(name = "HelloWorldServlet", urlPatterns = "/")
-public class HelloWorldServlet extends HttpServlet {
+@WebServlet(name = "IndexServlet", urlPatterns = "/")
+public class IndexServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        out.println("<h1>Hello, World!</h1>");
+        request.getRequestDispatcher("/WEB-INF/index.jsp").forward(request, response);
     }
 
 }
