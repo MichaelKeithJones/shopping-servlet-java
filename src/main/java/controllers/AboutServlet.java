c77b68e7f6f4ab921b0d7c798184d0ebc083b095
Michael Jones <michael.k.jones.20@gmail.com>
2022-04-27 11:38:45 -0500

    About controller and view created.

---
+++ src/main/java/controllers/AboutServlet.java
@@ -0,0 +1,16 @@
+package controllers;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "AboutServlet", urlPatterns = "/about")
+public class AboutServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/WEB-INF/about.jsp").forward(request, response);
+    }
+
+}
