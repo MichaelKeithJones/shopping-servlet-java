d25fe9c6617a97d2468f13a2169cea558cb37b7a
Michael Jones <michael.k.jones.20@gmail.com>
2022-04-29 18:54:49 -0500

    Update seeder.sql

--- seeder.sql
+++ seeder.sql
@@ -45,21 +45,21 @@ INSERT INTO products (item_id, color_id, height, width, length, dimension_unit, 
 (4, 3, 0.50, 2.30, 0.30, 'in', 2.00, 'oz', 'Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life.', 60.00),
 (4, 4, 0.50, 2.30, 0.30, 'in', 2.00, 'oz', 'Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life.', 60.00),
 
-(5, 2, 14.00, 12.00, 15.00, 'in', 19.00, 'oz', 'Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life.', 45.00),
-(5, 3, 78.00, 15.00, 19.00, 'in', 56.00, 'oz', 'Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life.', 50.00),
-(5, 4, 88.00, 23.00, 55.00, 'in', 12.00, 'oz', 'Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life.', 55.00),
+(5, 2, 8.00, 22.00, 15.00, 'in', 12.00, 'oz', 'Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life.', 50.00),
+(5, 3, 8.00, 22.00, 15.00, 'in', 12.00, 'oz', 'Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life.', 50.00),
+(5, 4, 8.00, 22.00, 15.00, 'in', 12.00, 'oz', 'Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life.', 50.00),
 
-(6, 1, 14.00, 12.00, 15.00, 'in', 19.00, 'oz', 'Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life.', 45.00),
+(6, 1, 45.00, 23.00, 88.00, 'in', 56.00, 'oz', 'Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life.', 60.00),
 
-(7, 1, 14.00, 12.00, 15.00, 'in', 19.00, 'oz', 'Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life.', 45.00),
+(7, 1, 4.50, 15.59, 8.60, 'in', 2.80, 'oz', 'Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life.', 40.00),
 
-(8, 1, 14.00, 12.00, 15.00, 'in', 19.00, 'oz', 'Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life.', 45.00),
-(8, 2, 78.00, 15.00, 19.00, 'in', 56.00, 'oz', 'Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life.', 50.00),
-(8, 3, 88.00, 23.00, 55.00, 'in', 12.00, 'oz', 'Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life.', 55.00),
+(8, 2, 32.00, 36.00, 54.00, 'in', 45.00, 'oz', 'Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life.', 60.00),
+(8, 3, 32.00, 36.00, 54.00, 'in', 45.00, 'oz', 'Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life.', 60.00),
+(8, 4, 32.00, 36.00, 54.00, 'in', 45.00, 'oz', 'Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life.', 60.00),
 
-(9, 1, 14.00, 12.00, 15.00, 'in', 19.00, 'oz', 'Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life.', 45.00),
-(9, 2, 78.00, 15.00, 19.00, 'in', 56.00, 'oz', 'Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life.', 50.00),
-(9, 3, 88.00, 23.00, 55.00, 'in', 12.00, 'oz', 'Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life.', 55.00);
+(9, 2, 2.20, 12.00, 4.50, 'in', 35.00, 'oz', 'Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life.', 50.00),
+(9, 3, 2.20, 12.00, 4.50, 'in', 35.00, 'oz', 'Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life.', 50.00),
+(9, 4, 2.20, 12.00, 4.50, 'in', 35.00, 'oz', 'Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life.', 50.00);
 
 
 INSERT INTO carts (product_id, quantity) VALUES
