2091b3431e48554290342ac0be76a1c0c0e5639d
Michael Jones <michael.k.jones.20@gmail.com>
2022-04-29 20:06:46 -0500

    Updated DAO methods.

--- src/main/java/doas/ProductsDao.java
+++ src/main/java/doas/ProductsDao.java
@@ -8,7 +8,7 @@ import com.mysql.cj.jdbc.Driver;
 
 public class ProductsDao implements Products {
 
-    private Connection connection;
+    private final Connection connection;
 
     public ProductsDao(Config config) {
         try {
@@ -49,38 +49,106 @@ public class ProductsDao implements Products {
         return name;
     }
 
-    private 
-
-    @Override
-    public List<Product> all() {
+    private List<Product> buildProductList (ResultSet rs) throws SQLException{
         List<Product> products = new ArrayList();
-        try {
-            PreparedStatement statement = connection.prepareStatement("SELECT * FROM products");
-            ResultSet rs = statement.executeQuery();
-            while (rs.next()) {
-                products.add(new Product(
-                        rs.getLong("id"),
-                        getItemName(rs.getLong("item_id")),
-                        getColorName(rs.getLong("color_id")),
-                        rs.getDouble("height"),
-                        rs.getDouble("width"),
-                        rs.getDouble("length"),
-                        rs.getString("dimension_unit"),
-                        rs.getDouble("weight"),
-                        rs.getString("weight_unit"),
-                        rs.getString("description"),
-                        rs.getDouble("cost")
-                ));
-            }
-        } catch (SQLException e) {
-            System.out.println("Could not get all products.");
+        while (rs.next()) {
+            products.add(new Product(
+                    rs.getLong("id"),
+                    getItemName(rs.getLong("item_id")),
+                    getColorName(rs.getLong("color_id")),
+                    rs.getDouble("height"),
+                    rs.getDouble("width"),
+                    rs.getDouble("length"),
+                    rs.getString("dimension_unit"),
+                    rs.getDouble("weight"),
+                    rs.getString("weight_unit"),
+                    rs.getString("description"),
+                    rs.getDouble("cost")
+            ));
         }
         return products;
     }
 
+    private Map<Product, Long> buildCartList (ResultSet rs) throws SQLException{
+        Map<Product, Long> cart = new HashMap<>();
+        while (rs.next()) {
+            cart.put(new Product(
+                    rs.getLong("id"),
+                    getItemName(rs.getLong("item_id")),
+                    getColorName(rs.getLong("color_id")),
+                    rs.getDouble("height"),
+                    rs.getDouble("width"),
+                    rs.getDouble("length"),
+                    rs.getString("dimension_unit"),
+                    rs.getDouble("weight"),
+                    rs.getString("weight_unit"),
+                    rs.getString("description"),
+                    rs.getDouble("cost")
+            ), rs.getLong("quantity") );
+        }
+        return cart;
+    }
+
+    @Override
+    public List<Product> all() {
+        try {
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM products");
+            return buildProductList(statement.executeQuery());
+        } catch (SQLException e) {
+            throw new RuntimeException("//-----| Error: Could not GET all products |-----//", e);
+        }
+    }
+
     // Todo: List first of all products list, limit: 3      // select * from products group by item_id limit 3;
+    public List<Product> top3() {
+        try {
+            PreparedStatement statement = connection.prepareStatement("select * from products group by item_id limit 3");
+            return buildProductList(statement.executeQuery());
+        } catch (SQLException e) {
+            throw new RuntimeException("//-----| Error: Could not GET all products |-----//", e);
+        }
+    }
+
     // Todo: List first of all products list                // select * from products group by item_id;
+    public List<Product> first() {
+        try {
+            PreparedStatement statement = connection.prepareStatement("select * from products group by item_id");
+            return buildProductList(statement.executeQuery());
+        } catch (SQLException e) {
+            throw new RuntimeException("//-----| Error: Could not GET all products |-----//", e);
+        }
+    }
+
     // Todo: List all product by item                       // select * from products where item_id = 1;
-    // Todo: List all products by category                  // select *from products where item_id in (select item_id from items where item_id in (select item_id from items_categories where category_id = 4)) group by item_id;
+    public List<Product> findByItemId(long id) {
+        try {
+            PreparedStatement statement = connection.prepareStatement("select * from products where item_id = ?");
+            statement.setLong(1, id);
+            return buildProductList(statement.executeQuery());
+        } catch (SQLException e) {
+            throw new RuntimeException("//-----| Error: Could not GET all products |-----//", e);
+        }
+    }
+
+    // Todo: List all products by category                  // select * from products where item_id in (select item_id from items where item_id in (select item_id from items_categories where category_id = 4)) group by item_id;
+    public List<Product> findByCategoryId(long id) {
+        try {
+            PreparedStatement statement = connection.prepareStatement("select * from products where item_id in (select item_id from items where item_id in (select item_id from items_categories where category_id = ?)) group by item_id;");
+            statement.setLong(1, id);
+            return buildProductList(statement.executeQuery());
+        } catch (SQLException e) {
+            throw new RuntimeException("//-----| Error: Could not GET all products |-----//", e);
+        }
+    }
+
     // Todo: List all products from shopping cart           // select p.*, c.quantity from products as p join carts as c on c.product_id = p.id where p.id in (select c.id from carts where c.product_id = p.id);
+    public Map<Product, Long> getCart() {
+        try {
+            PreparedStatement statement = connection.prepareStatement("select p.*, c.quantity from products as p join carts as c on c.product_id = p.id where p.id in (select c.id from carts where c.product_id = p.id)");
+            return buildCartList(statement.executeQuery());
+        } catch (SQLException e) {
+            throw new RuntimeException("//-----| Error: Could not GET all products |-----//", e);
+        }
+    }
+
 }
