46bf59c6767f1a296dd1d9d74889ba99bf295fc2
Michael Jones <michael.k.jones.20@gmail.com>
2022-05-01 14:08:36 -0500

    Update IndexServlet.java

--- src/main/java/controllers/IndexServlet.java
+++ src/main/java/controllers/IndexServlet.java
@@ -1,10 +1,6 @@
 package controllers;
 
-import doas.DaoFactory;
-import models.Cart;
-
 import java.io.*;
-import java.util.List;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
@@ -14,15 +10,6 @@ public class IndexServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        long id = DaoFactory.getCartDao().checkCart(3);
-        System.out.println("id = " + id);
-
-        List<Cart> list = DaoFactory.getCartDao().all();
-        System.out.println("list.get(0).getName() = " + list.get(0).getName());
-        System.out.println("list.get(0).getName() = " + list.get(0).getColor());
-        System.out.println("list.get(0).getName() = " + list.get(0).getCost());
-        System.out.println("list.get(0).getName() = " + list.get(0).getQuantity());
-
         request.getRequestDispatcher("/WEB-INF/index.jsp").forward(request, response);
     }
 
